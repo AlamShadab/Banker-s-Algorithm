81cc80ffaf75da98574af3679d921ef05bbe4215
mrajaj <49529509+mrajaj@users.noreply.github.com>
2019-04-12 01:18:36 +0530

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+
+int nResources,
+    nProcesses;
+int *resources;
+int **allocated;
+int **maxRequired;
+int **need;
+int *safeSeq;
+int nProcessRan = 0;
+pthread_mutex_t lockResources;
